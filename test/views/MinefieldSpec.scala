b477b7aa820ebece04bb97dfc7a33d04b3aa6756
Pete1232 <pete1232@gmail.com>
2015-11-24 12:52:07 +0000

    Created minefield view

--- test/views/MinefieldSpec.scala
+++ test/views/MinefieldSpec.scala
@@ -1,10 +1,25 @@
 package views
 
 import org.scalatest.{Matchers, FlatSpec}
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.test.FakeApplication
 
 /**
   * Created by pnewman on 24/11/2015.
   */
 class MinefieldSpec extends FlatSpec with Matchers{
 
+  def testMinefield: Unit ={
+
+    lazy val html = views.html.minefield()
+
+    "The minefield view" should "rander an html page" in{
+      running(new FakeApplication()){
+        contentType(html) should be ("text/html")
+      }
+    }
+  }
+
+  testMinefield
 }
