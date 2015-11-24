96e1cde72436df4ffd56765a605d7cf4a44a5223
Pete1232 <pete1232@gmail.com>
2015-11-24 16:51:51 +0000

    Added table with buttons in it

--- test/views/MinefieldSpec.scala
+++ test/views/MinefieldSpec.scala
@@ -15,20 +15,27 @@ class MinefieldSpec extends FlatSpec with Matchers{
     lazy val html = views.html.minefield()
 
     "The minefield view" should "render an html page" in{
-      running(new FakeApplication()){
+      new FakeApplication(){
         contentType(html) should be ("text/html")
       }
     }
 
-    it should "include a table" in {
-      contentAsString(html) should include("<table>")
-      contentAsString(html) should include("</table>")
+    it should "be embedded in the main view with title \"Minesweeper\" " in {
+      new FakeApplication() {
+        contentAsString(html) should include("<title>Minesweeper</title>")
+
+      }
     }
 
-    it should "be embedded in the main view with title \"Minesweeper\" " in {
-      contentAsString(html) should include("@main(\"Minesweeper\")")
+    it should "include a table" in {
+      contentAsString(html) should include("<table class=\"table table-bordered table-hover\">")
+    }
+
+    it should "have at least one table cell" in {
+      contentAsString(html) should include ("<td>")
     }
 
   }
+
   testMinefield
 }
